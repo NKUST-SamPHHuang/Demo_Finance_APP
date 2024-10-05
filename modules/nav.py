72cd3f12a47608e70cf7d31d0200a3f4273b2b20
DESKTOP-HJ03U5R\samch <samphhuang@nkust.edu.tw>
2024-10-05 23:14:13 +0800

    修改檔名

--- modules/nav.py
+++ modules/nav.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 def Navbar():
     with st.sidebar:
-        st.page_link('Finance Evaluation System.py', label='投資組合評估系統', icon='🔥')
-        st.page_link('pages/1. Charts.py', label='股價繪圖', icon='🛡️')
-        st.page_link('pages/2. Portfolio Selections.py', label='自選投資組合', icon='🛡️')
-        st.page_link('pages/3. Portfolio Analysis.py', label='投資組合分析', icon='🛡️')
+        st.page_link('Finance_Evaluation_System.py', label='投資組合評估系統', icon='🔥')
+        st.page_link('pages/Charts.py', label='股價繪圖', icon='🛡️')
+        st.page_link('pages/Portfolio_Selections.py', label='自選投資組合', icon='🛡️')
+        st.page_link('pages/Portfolio_Analysis.py', label='投資組合分析', icon='🛡️')
 
 
 
